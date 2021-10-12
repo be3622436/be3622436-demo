922e75a6d443382529d395efc54a04181e7bb4f2
be3622436 <be3622436@gmail.com>
2021-10-12 16:58:06 +0800

    mark security code

--- main-demo/src/main/java/com/example/kracedemo/interceptor/LoginInterceptor.java
+++ main-demo/src/main/java/com/example/kracedemo/interceptor/LoginInterceptor.java
@@ -1,35 +1,35 @@
-//package com.example.kracedemo.interceptor;
-//
-//import com.example.kracedemo.entity.mysql.User;
-//import com.example.kracedemo.exception.common.NotLoginAccessException;
-//
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import javax.servlet.http.HttpSession;
-//
-//@Aspect
-//@Component
-//public class LoginInterceptor {
-//  @Autowired private HttpSession session;
-//
-//  @Pointcut(
-//      "within(com.example.kracedemo.controller.HomeController) || "
-//          + "within(com.example.kracedemo.controller.board.*) || "
-//          + "within(com.example.kracedemo.controller.user.*)")
-//  public void requiredLoginPointCut() {}
-//
-//  @Before("requiredLoginPointCut()")
-//  public void checkLogin(JoinPoint joinPoint) {
-//    User user = (User) session.getAttribute("user");
-//    if (user == null) {
-//      System.out.println("-----------user did not login-----------");
-//      throw new NotLoginAccessException();
-//    } else {
-//      System.out.println("-----------user already login-----------");
-//    }
-//  }
-//}
+package com.example.kracedemo.interceptor;
+
+import com.example.kracedemo.entity.mysql.User;
+import com.example.kracedemo.exception.common.NotLoginAccessException;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpSession;
+
+@Aspect
+@Component
+public class LoginInterceptor {
+  @Autowired private HttpSession session;
+
+  @Pointcut(
+      "within(com.example.kracedemo.controller.HomeController) || "
+          + "within(com.example.kracedemo.controller.board.*) || "
+          + "within(com.example.kracedemo.controller.user.*)")
+  public void requiredLoginPointCut() {}
+
+  @Before("requiredLoginPointCut()")
+  public void checkLogin(JoinPoint joinPoint) {
+    User user = (User) session.getAttribute("user");
+    if (user == null) {
+      System.out.println("-----------user did not login-----------");
+      throw new NotLoginAccessException();
+    } else {
+      System.out.println("-----------user already login-----------");
+    }
+  }
+}
